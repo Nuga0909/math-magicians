51dad5db29ca73e10c2179d0e00c236e72359488
Adeshina Adenuga <Nuga0909@gmail.com>
2023-02-15 15:41:03 +0100

    Add event listener and onclick functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,65 @@
 import React, { useState } from 'react';
-import calculate from "../logic/calculate";
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="container flex">
-    <div className="Output-bar flex button-container"> 0</div>
-    <div className="buttons">
-      <div className="col-1 button-container flex">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="colored">÷</button>
-      </div>
-      <div className="col-2 button-container flex">
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button">x</button>
-      </div>
+const Calculator = () => {
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-      <div className="col-3 button-container flex">
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button">-</button>
+  const clickEvent = (e) => {
+    const buttonName = e.currentTarget.textContent;
+    const newState = calculate(calculation, buttonName);
+    setCalculation(newState);
+  };
+
+  const { next, operation, total } = calculation;
+
+  return (
+    <div className="container flex">
+      <div className="Output-bar flex button-container">
+        { total }
+        {' '}
+        { operation }
+        {' '}
+        { next }
       </div>
-      <div className="col-4 button-container flex">
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">+</button>
-      </div>
-      <div className="col-5 button-container flex">
-        <button type="button">0</button>
-        <button type="button">
-          .
-        </button>
-        <button type="button">=</button>
+      <div className="buttons">
+        <div className="col-1 button-container flex">
+          <button type="button" name="AC" onClick={clickEvent}>AC</button>
+          <button type="button" name="+/-" onClick={clickEvent}>+/-</button>
+          <button type="button" name="%" onClick={clickEvent}>%</button>
+          <button type="button" name="÷" onClick={clickEvent}>÷</button>
+        </div>
+        <div className="col-2 button-container flex">
+          <button type="button" name="7" onClick={clickEvent}>7</button>
+          <button type="button" name="8" onClick={clickEvent}>8</button>
+          <button type="button" name="9" onClick={clickEvent}>9</button>
+          <button type="button" name="x" onClick={clickEvent}>X</button>
+        </div>
+
+        <div className="col-3 button-container flex">
+          <button type="button" name="4" onClick={clickEvent}>4</button>
+          <button type="button" name="5" onClick={clickEvent}>5</button>
+          <button type="button" name="6" onClick={clickEvent}>6</button>
+          <button type="button" name="-" onClick={clickEvent}>-</button>
+        </div>
+        <div className="col-4 button-container flex">
+          <button type="button" name="1" onClick={clickEvent}>1</button>
+          <button type="button" name="2" onClick={clickEvent}>2</button>
+          <button type="button" name="3" onClick={clickEvent}>3</button>
+          <button type="button" name="+" onClick={clickEvent}>+</button>
+        </div>
+        <div className="col-5 button-container flex">
+          <button onClick={clickEvent} name="0" type="button">0</button>
+          <button onClick={clickEvent} name="." type="button">
+            .
+          </button>
+          <button onClick={clickEvent} name="=" type="button">=</button>
+        </div>
       </div>
     </div>
-  </div>
-
-);
-
+  );
+};
 export default Calculator;
